d7754700dafa94548a21e9a333e9c2402a881e93
ErwinMcdiz <erwinmacahilo01@gmail.com>
2025-05-02 20:00:57 +0800

    admin users Crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :traders, only: [:index, :new, :create, :edit, :update, :destroy, :show]
     get "dashboard", to: "dashboard#index"
   end
 
