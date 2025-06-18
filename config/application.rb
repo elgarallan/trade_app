a352ab96f2d252461b75581bdd2bd124de20c3f7
elgarallan <161586446+elgarallan@users.noreply.github.com>
2025-06-18 16:28:27 +0800

    delete config/queue

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module TradeApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join("wrappers")
+    config.active_job.queue_adapter = :async
   end
 end
