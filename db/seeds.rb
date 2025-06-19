ae72494e295caf2ecccc560d9ca5f51e66bfab1a
elgarallan <161586446+elgarallan@users.noreply.github.com>
2025-06-19 20:54:41 +0800

    Mark admin user as approved in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,6 @@ user = User.find_or_initialize_by(email: "admin@avion.com")
 user.password = "password123"
 user.password_confirmation = "password123"
 user.admin = true
+user.approved = true
 user.save!
-puts "Seeded admin: #{user.email}, Admin?: #{user.admin}"
+puts "Seeded admin: #{user.email}, Admin?: #{user.admin}, Approved?: #{user.approved}"
